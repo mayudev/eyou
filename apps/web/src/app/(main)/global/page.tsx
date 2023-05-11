2d3546d4d007e5979527e1fd785e16ff99af9fba
mayudev <99561108+mayudev@users.noreply.github.com>
2023-05-11 20:49:12 +0200

    chore: add missing pages

---
+++ apps/web/src/app/(main)/global/page.tsx
@@ -0,0 +1,3 @@
+export default function GlobalPage() {
+  return <div>global</div>;
+}
---
+++ apps/web/src/app/(main)/settings/page.tsx
@@ -0,0 +1,4 @@
+// this will most likely have to be moved out of (main)
+export default function SettingsPage() {
+  return <div>settings</div>;
+}
