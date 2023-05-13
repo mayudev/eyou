91025d5b57039e4f958d1a0d47d75347dacc1025
mayudev <99561108+mayudev@users.noreply.github.com>
2023-05-13 16:29:12 +0200

    chore: implement `Account` and `Field`

--- packages/types/src/Account.ts
+++ packages/types/src/Account.ts
@@ -1,10 +1,31 @@
+import { Field } from "./Field";
+
 export interface BaseAccount {
   id: string;
   username: string;
   acct: string;
   url: string;
   display_name: string;
+  note: string;
   avatar: string;
+  avatar_static: string;
+  header: string;
+  header_static: string;
+  locked: boolean;
+  fields: Field[];
+  emojis: null; // TODO
+  bot: boolean;
+  group: boolean;
+  discoverable: boolean | null;
+  noindex?: boolean | null; // TODO (this is always t)
+  moved?: Account | null;
+  suspended?: boolean;
+  limited?: boolean;
+  created_at: string;
+  last_status_at?: string | null;
+  statuses_count: number;
+  followers_count: number;
+  following_count: number;
 }
 
 export interface Account extends BaseAccount {}
--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,1 +1,3 @@
 export type { BaseAccount, Account } from "./Account";
+export type { Status } from "./Status";
+export type { Field } from "./Field";
