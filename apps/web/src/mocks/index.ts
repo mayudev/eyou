ade7cc2cdc19e0f56bc8e954d7a1d7558c6964a2
mayudev <99561108+mayudev@users.noreply.github.com>
2023-05-17 16:49:12 +0200

    fix: bypass unhandled requests

--- apps/web/src/mocks/index.ts
+++ apps/web/src/mocks/index.ts
@@ -2,7 +2,9 @@ async function initMocks() {
   if (process.env.NODE_ENV === "development") {
     if (typeof window === "undefined") {
       const { server } = await import("./server");
-      server.listen();
+      server.listen({
+        onUnhandledRequest: "bypass",
+      });
     } else {
       const { worker } = await import("./browser");
       worker.start();
